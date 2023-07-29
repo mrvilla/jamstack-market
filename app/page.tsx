34ca7ece0bd8a3cd84d404c640f7108750b182d0
iammrvilla <3463688+mrvilla@users.noreply.github.com>
2023-07-29 16:30:46 +0200

    add: contentful data, graphql, add one trip

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,93 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 
-export default function Home() {
-  return (
+const getData = async () => {
+    const response = await fetch(`https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}/environments/master`, {
+        method: 'POST',
+        headers: {
+            Authorization: `Bearer ${process.env.CONTENTFUL_DELIVERY_TOKEN}`,
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify({
+            query: `
+                query {
+                  tripCollection {
+                    __typename
+                    items {
+                      __typename
+                      title
+                      image {
+                        __typename
+                        title
+                        url
+                      }
+                      destination {
+                        __typename
+                        lon
+                        lat
+                      }
+                    }
+                  }
+                }
+            `,
+        })
+    });
+
+    if (!response.ok) {
+        console.error(response);
+        return [];
+    }
+
+    const data = await response.json();
+    return data.data.tripCollection.items;
+    /**
+     query {
+      tripCollection {
+        __typename
+        items {
+          __typename
+          title
+          image {
+            __typename
+            title
+            url
+          }
+          destination {
+            __typename
+            lon
+            lat
+          }
+        }
+      }
+    }
+     */
+}
+
+export default async function Home({trips}) {
+    const data = await getData();
+
+    console.log('Data:', JSON.stringify(data, null, 2));
+
+    return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
+        <h1>
+          Jamstack trips collection
+        </h1>
         <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            {data.map((item) => (
+                <>
+                    <h4>{item.title}</h4>
+                    <Image
+                        src={item.image.url}
+                        alt={item.image.title}
+                        width={640}
+                        height={422}
+                        priority
+                    />
+s                </>
+            ))}
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
   )
 }
+
